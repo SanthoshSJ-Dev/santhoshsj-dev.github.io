ec8b6f1b6c343e1625574608ab8a636ac33d7bfd
Santhosh S <santhoshsekar2903@gmail.com>
2022-07-22 19:20:22 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-    window.setTimeout('fadeout();',3000);
-});
-
-function fadeout(){
-    $('#preloader').delay(2000).fadeout('slow',function(){
-        $('.body').removeClass('body');
-    });
-}
+setTimeout(function(){
+    $('.preloader').fadeToggle();
+}, 5000);
